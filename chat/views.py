0acdeca7f56af2b43763ba53f79f5cc73e199666
Shmygol_Maksim <sh.maksim343@gmail.com>
2025-03-21 17:56:20 +0500

    application skeleton

---
+++ chat/views.py
@@ -0,0 +1,16 @@
+from django.forms.models import model_to_dict
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from chat.models import Chat
+
+
+# Create your views here.
+class ChatAPIView(APIView):
+    def get(self, request):
+        return Response(Chat.objects.all().values())
+
+    def post(self, request):
+        new_chat = Chat.objects.create(title=request.POST['title'])
+        return Response({'chat': model_to_dict(new_chat)})
--- fort/settings.py
+++ fort/settings.py
@@ -12,9 +12,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -31,11 +36,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat.apps.ChatConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +83,12 @@ WSGI_APPLICATION = 'fort.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
