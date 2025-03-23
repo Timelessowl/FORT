1bdd55d0b38cdac67f71ae257905f13ff67b1384
Shmygol_Maksim <sh.maksim343@gmail.com>
2025-03-23 23:25:47 +0500

    implement POST /chat route

--- back/chat/views.py
+++ back/chat/views.py
@@ -1,15 +1,34 @@
-from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+import logging
+import uuid
 
-from back.chat.models import Chat
+logger = logging.getLogger(__name__)
 
 
-# Create your views here.
 class ChatAPIView(APIView):
-    def get(self, request):
-        return Response(Chat.objects.all().values())
-
     def post(self, request):
-        new_chat = Chat.objects.create(title=request.POST['title'])
-        return Response({'chat': model_to_dict(new_chat)})
+        try:
+            token = request.data.get('token')
+            text = request.data.get('text')
+
+            if not text:
+                return Response({'error': 'The "text" field is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+            if not token:
+                token = str(uuid.uuid4())
+
+            # ============================================== Вызов агента ==============================================
+            response_agent = f'call_agent({token}, {text})'
+            # ============================================== Вызов агента ==============================================
+
+            return Response({'token': token, 'text': response_agent}, status=status.HTTP_200_OK)
+
+        except ValueError as ve:
+            logger.warning(f'Agent error: {ve}')
+            return Response({'error': 'Agent error'}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            logger.exception(f'Error contacting agent: {e}')
+            return Response({'error': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
