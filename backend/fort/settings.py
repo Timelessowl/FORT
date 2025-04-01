638545f364b84b38e81f494f0e17a3e7905dbc30
Shmygol_Maksim <sh.maksim343@gmail.com>
2025-04-01 20:05:50 +0500

    added cors

--- backend/fort/settings.py
+++ backend/fort/settings.py
@@ -30,7 +30,25 @@ SECRET_KEY = 'django-insecure-h=2(9qbc-!bn86pz12m7^jg7@p!wl*4@y(ai@^qeg*kq1q0v_n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     # Указать домены и отключить CORS_ALLOW_ALL_ORIGINS
+# ]
+CORS_ALLOW_METHODS = [
+    # 'DELETE',
+    'GET',
+    # 'OPTIONS',
+    # 'PATCH',
+    'POST',
+    # 'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'authorization',
+    'content-type',
+    'origin',
+    'x-requested-with',
+]
 
 
 # Application definition
@@ -61,11 +79,13 @@ INSTALLED_APPS = [
     'mermaid.apps.MermaidConfig',
     'rest_framework',
     'drf_spectacular',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
