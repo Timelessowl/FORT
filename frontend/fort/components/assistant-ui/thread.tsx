55c10fe264928349d86ca9e5f2589fffb83db2a3
timelessowl <timelessowl@debian.deb>
2025-05-08 11:49:02 +0500

    ref: ui and schemes message

--- frontend/fort/components/assistant-ui/thread.tsx
+++ frontend/fort/components/assistant-ui/thread.tsx
@@ -20,6 +20,7 @@ import {
   ImageIcon,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { ThreadBackgroundMessage } from "@/lib/messages";
 
 import { Button } from "@/components/ui/button";
 import { MarkdownText } from "@/components/assistant-ui/markdown-text";
@@ -95,7 +96,9 @@ const ThreadWelcome: FC<{ stageIndex: number }> = ({ stageIndex }) => {
     <ThreadPrimitive.Empty>
       <div className="flex w-full max-w-[var(--thread-max-width)] flex-grow flex-col">
         <div className="flex w-full flex-grow flex-col items-center justify-center">
-          <p className="mt-4 font-medium">Фигово описали, разберемся потом :) </p>
+          {stageIndex === 4 ? 
+              <p className="mt-4 font-medium whitespace-pre-line">{ThreadBackgroundMessage}</p>
+              : <p className="mt-4 font-medium">Фигово описали, разберемся потом :) </p> } 
         </div>
         { /*<ThreadWelcomeSuggestions />*/ }
       </div>
